fdffaab026ee202ca89864c0c7a440cd64e4406b
ErickCReis <erickcorreareis@gmail.com>
2020-12-09 13:58:41 -0300

    :sparkles:  authenticated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-// import { Container } from './styles';
+import DashBoard from '../pages/DashBoard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={DashBoard} isPrivate />
   </Switch>
 );
 
